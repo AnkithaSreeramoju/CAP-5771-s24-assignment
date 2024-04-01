58afcb50c754f095cc166182b40a552d8ac345e1
AnkithaSreeramoju <83494220+AnkithaSreeramoju@users.noreply.github.com>
2024-03-31 21:57:45 -0400

    Update part1.py

--- part1.py
+++ part1.py
@@ -29,8 +29,17 @@ In the first task, you will explore how k-Means perform on datasets with diverse
 # Change the arguments and return according to 
 # the question asked. 
 
-def fit_kmeans():
-    return None
+from sklearn.datasets import make_circles, make_moons, make_blobs
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+
+def fit_kmeans(dataset, n_clusters, random_state=42):
+    X = dataset[0]
+    scaler = StandardScaler()
+    X_normalized = scaler.fit_transform(X)
+    kmeans = KMeans(n_clusters=n_clusters, init='k-means++', random_state=random_state)
+    kmeans.fit(X_normalized)
+    return kmeans.labels_
 
 
 def compute():
@@ -39,10 +48,22 @@ def compute():
     """
     A.	Load the following 5 datasets with 100 samples each: noisy_circles (nc), noisy_moons (nm), blobs with varied variances (bvv), Anisotropicly distributed data (add), blobs (b). Use the parameters from (https://scikit-learn.org/stable/auto_examples/cluster/plot_cluster_comparison.html), with any random state. (with random_state = 42). Not setting the correct random_state will prevent me from checking your results.
     """
+    n_samples = 100
+    random_state = 42
+    noisy_circles = make_circles(n_samples=100, factor=0.5, noise=0.05)
+    noisy_moons = make_moons(n_samples=100, noise=0.05)
+    blobs_varied = make_blobs(n_samples=100, cluster_std=[1.0, 2.5, 0.5], random_state=170)
+    aniso = (np.dot(blobs_varied[0], [[0.6, -0.6], [-0.4, 0.8]]), blobs_varied[1])
+    blobs = make_blobs(n_samples=100, random_state=8)
 
+    datasets = {'nc': noisy_circles, 'nm': noisy_moons, 'bvv': blobs_varied, 'add': aniso, 'b': blobs}
     # Dictionary of 5 datasets. e.g., dct["nc"] = [data, labels]
     # 'nc', 'nm', 'bvv', 'add', 'b'. keys: 'nc', 'nm', 'bvv', 'add', 'b' (abbreviated datasets)
-    dct = answers["1A: datasets"] = {}
+    dct = answers["1A: datasets"] = {'nc': [noisy_circles[0],noisy_circles[1]],
+                                     'nm': [noisy_moons[0],noisy_moons[1]],
+                                     'bvv': [blobs_varied[0],blobs_varied[1]],
+                                     'add': [aniso[0],aniso[1]],
+                                     'b': [blobs[0],blobs[1]]}
 
     """
    B. Write a function called fit_kmeans that takes dataset (before any processing on it), i.e., pair of (data, label) Numpy arrays, and the number of clusters as arguments, and returns the predicted labels from k-means clustering. Use the init='random' argument and make sure to standardize the data (see StandardScaler transform), prior to fitting the KMeans estimator. This is the function you will use in the following questions. 
